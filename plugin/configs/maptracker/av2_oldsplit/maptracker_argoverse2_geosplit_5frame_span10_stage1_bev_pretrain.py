2cea534de0bc682e049e2f7bb8c494b74edf9af6
Shen Zheng <shenzhen@trinity-2-25.eth>
2025-09-16 13:03:09 -0400

    9_16_v1 fix eval_config path errors

--- plugin/configs/maptracker/av2_oldsplit/maptracker_argoverse2_geosplit_5frame_span10_stage1_bev_pretrain.py
+++ plugin/configs/maptracker/av2_oldsplit/maptracker_argoverse2_geosplit_5frame_span10_stage1_bev_pretrain.py
@@ -2,23 +2,35 @@ _base_ = [
     'maptracker_av2_oldsplit_5frame_span10_stage1_bev_pretrain.py'
 ]
 
+# overwrite PKL paths everywhere
+new_val_pkl = './datasets/argoverse2_geosplit/av2_map_infos_val.pkl'
+new_train_pkl = './datasets/argoverse2_geosplit/av2_map_infos_train.pkl'
+
 # dataset overrides for geo-split
 data = dict(
     train=dict(
-        ann_file='./datasets/argoverse2_geosplit/av2_map_infos_train.pkl',
+        ann_file=new_train_pkl,
     ),
     val=dict(
-        ann_file='./datasets/argoverse2_geosplit/av2_map_infos_val.pkl',
+        ann_file=new_val_pkl,
+        eval_config=dict(
+            ann_file=new_val_pkl,   # <--- must override here too
+        ),
     ),
     test=dict(
-        ann_file='./datasets/argoverse2_geosplit/av2_map_infos_val.pkl',
+        ann_file=new_val_pkl,
+        eval_config=dict(
+            ann_file=new_val_pkl,   # <--- must override here too
+        ),
     ),
 )
 
+# also override top-level eval_config (for safety)
 eval_config = dict(
-    ann_file='./datasets/argoverse2_geosplit/av2_map_infos_val.pkl'
+    ann_file=new_val_pkl
 )
 
+# and match_config if required
 match_config = dict(
-    ann_file='./datasets/argoverse2_geosplit/av2_map_infos_val.pkl'
+    ann_file=new_val_pkl,
 )
